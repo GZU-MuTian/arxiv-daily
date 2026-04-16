6c9da59eef0a65024abdf0aaa97343cdfa64c2d6
yuliu <yuliu@gzu.edu.cn>
2026-04-16 08:07:51 +0000

    Add extractpr

--- src/arxiv_daily/__init__.py
+++ src/arxiv_daily/__init__.py
@@ -1,8 +1,9 @@
-from .core import _run_new, _run_summarize
+from .core import _run_new, _run_summarize, _run_extractor
 
 
 arxiv_new = _run_new
 arxiv_summarize = _run_summarize
+arxiv_extractor = _run_extractor
 
 
-__all__ = ["arxiv_new", "arxiv_summarize"]
+__all__ = ["arxiv_new", "arxiv_summarize", "arxiv_extractor"]
